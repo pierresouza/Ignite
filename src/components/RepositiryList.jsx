e15a42d2f20f768cc6f91755ee0b8b47c7bb8d89
Pierre Souza <pas@lionx.com.br>
2022-07-04 18:02:25 -0300

    update

--- src/components/RepositiryList.jsx
+++ src/components/RepositiryList.jsx
@@ -1,13 +1,27 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RespositoryItem";
+
+import "../styles/repositories.scss";
+
+//https://api.github.com/orgs/rocketseat/repos
 
 const repository = {
   name: "unform",
   description: "Forms in React",
   link: "https://github.com/unform/unform",
 };
+
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((Response) => Response.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
-    <section className="respository-list">
+    <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
