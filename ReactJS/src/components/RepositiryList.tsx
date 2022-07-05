1000a8c0ea3ec8dca4e21113d149417a5f7a1700
Pierre Souza <pas@lionx.com.br>
2022-07-05 15:51:15 -0300

    update

--- ReactJS/src/components/RepositiryList.tsx
+++ ReactJS/src/components/RepositiryList.tsx
@@ -3,11 +3,17 @@ import { RepositoryItem } from "./RespositoryItem";
 
 import "../styles/repositories.scss";
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
-    fetch("https://api.github.com/orgs/rocketseat/repos")
+    fetch("https://api.github.com/users/pierresouza/repos")
       .then((Response) => Response.json())
       .then((data) => setRepositories(data));
   }, []);
@@ -18,7 +24,9 @@ export function RepositoryList() {
 
       <ul>
         {repositories.map((repository) => {
-          return <RepositoryItem key={repository.id} repository={repository} />;
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
         })}
       </ul>
     </section>
